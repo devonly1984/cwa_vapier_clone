6e3447c8ff4fee133f1d79deed8568e18ece4459
pcguard <pcguard@PON2027493D.safeway01.ad.safeway.com>
2025-11-09 07:44:39 -0600

    Added TRPC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
+import { TRPCReactProvider } from "@/lib/trpc/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ const  RootLayout=({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-const HomePage = () => {
+import prisma from "@/lib/prisma/client"
+
+const HomePage = async () => {
+  const users = await prisma.user.findMany();
   return (
-    <div>HomePage</div>
-  )
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
+    </div>
+  );
 }
 export default HomePage
